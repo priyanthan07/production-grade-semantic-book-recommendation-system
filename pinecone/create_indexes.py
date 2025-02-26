f0b2a311c47156671f8f0175390baebc07e290ce
LAPTOP-JNT30UUH\Salus X Labs <govindarajpriyanthan@gmail.com>
2025-02-26 21:04:49 +0530

    feat(pinecone): add script for index creation and deletion
    Introduced a new script `create_indexes.py` to facilitate the creation and deletion of Pinecone indexes. The script uses environment variables for authentication and specifies index parameters such as dimension, metric, and deployment region. This addition simplifies managing Pinecone resources programmatically within a development environment.

---
+++ pinecone/create_indexes.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+from pinecone.grpc import PineconeGRPC as Pinecone
+from pinecone import ServerlessSpec
+
+load_dotenv()
+
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+
+def create_indexes(index_name):
+    pc.create_index(
+        name=index_name,
+        dimension=1536,
+        metric="cosine",
+        spec=ServerlessSpec(
+            cloud="aws",
+            region="us-east-1"
+        ),
+        deletion_protection="disabled",
+        tags={
+            "environment": "development"
+        }
+    )
+    print(f"Index {index_name} created successfully. {pc.describe_index(index_name)}")
+    
+def delete_indexes(index_name):
+    pc.delete_index(index_name)
+    print(f"Index {index_name} deleted successfully.")
+
+
+if __name__ == "__main__":
+    
+# ==========================================================================
+# Create book-index-1
+# ==========================================================================
+
+    create_indexes("book-index1")
+
+# ==========================================================================
+# Create book-index-1
+# ==========================================================================
+
+    create_indexes("book-index2")
